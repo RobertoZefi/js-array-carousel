894eb594e0df4ec832f49e56e09cb3c1b2ddc0cf
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-06 18:09:38 +0100

    js

--- app.js
+++ app.js
@@ -25,8 +25,13 @@ arrowRight.addEventListener('click', function() {
     
     console.log(slideCounter)
 
-    if (slideCounter === slideElement.length){
-        slideSuccessiva = slideElement[0]
+    if (slideCounter == 4){
+        slideCounter = 0
+
+        slideCorrente = slideElement[slideCounter]
+        slideCorrente.classList.remove('active')
+
+        slideSuccessiva = slideElement[slideCounter]
         slideSuccessiva.classList.add('active')
     }
 
