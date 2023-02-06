613ec870ec42d1329743d2fedcaaffa6c0104611
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-06 23:27:20 +0100

    FEAT milestone 3 added

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+const slides = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+  ]
+
+const carosello = document.querySelector('.carosello')
+
+let figure = `
+    <div class="slide active">
+        <img src="${slides[0]}" alt="">
+    </div>
+    <div class="slide">
+        <img src="${slides[1]}" alt="">
+    </div>
+    <div class="slide">
+        <img src="${slides[2]}" alt="">
+    </div>
+    <div class="slide">
+        <img src="${slides[3]}" alt="">
+    </div>
+    <div class="slide">
+        <img src="${slides[4]}" alt="">
+    </div>
+    <i class="arrow arrow__left fa-solid fa-chevron-left"></i>
+    <i class="arrow arrow__right fa-solid fa-chevron-right"></i>  
+`
+carosello.innerHTML += figure 
+
+
+
 console.log('daje')
 
 const slideElement = document.getElementsByClassName('slide')
@@ -11,15 +44,14 @@ console.log(arrowRight)
 let slideCounter = 0
 
 
-
 arrowRight.addEventListener('click', function() {
-    console.log(arrowRight, slideElement.length)
+    console.log('Hai schiacciato', arrowRight, slideElement.length)
 
     let slideCorrente = slideElement[slideCounter]
     slideCorrente.classList.remove('active')
 
-    if (slideCounter === 4){
-        slideCounter=0
+    if (slideCounter === slideElement.length - 1){
+        slideCounter = 0
     } else {
         slideCounter++
     }
@@ -40,7 +72,7 @@ arrowLeft.addEventListener('click', function() {
     slideCorrente.classList.remove('active')
 
     if (slideCounter === 0){
-        slideCounter=4
+        slideCounter = slideElement.length - 1
     } else {
         slideCounter--
     }
@@ -52,3 +84,16 @@ arrowLeft.addEventListener('click', function() {
     
     console.log(slideCounter)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
